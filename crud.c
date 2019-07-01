ea3777868f38f6b2301039e0179140ba40e182ac
alextorrer <alextorre0427@gmail.com>
2019-06-30 22:50:18 -0500

    Add person struct Add a person CRUD Documentation

--- crud.c
+++ crud.c
@@ -5,16 +5,22 @@
 
 /*Constants */
 const int MAX = 100;
+const int PEOPLENUM = 3;
 #define TRUE 1
 #define FALSE 0
 
-/*Structures*/
+/**
+*@struct Date struct
+*/
 struct Date{
     int day;
     int month;
     int year;
 };
 
+/**
+*@struct Activity struct
+*/
 struct Activity{
     int id;
     char *title;
@@ -24,14 +30,28 @@ struct Activity{
     struct Date finish;
 };
 
+/**
+*@struct List struct
+*/
 struct List{
     int index;
     struct Node *head;
 };
 
+/**
+*@struct Node struct
+*/
 struct Node{
 	struct Activity *activity;
 	struct Node *next;
+};
+
+/**
+*@struct Person struct
+*/
+struct Person{
+	struct List *list;
+	char *name;
 };
 
 /*Functions prototypes */
@@ -54,6 +74,11 @@ int getLeapYears(int year);
 int isLeapYear(int year);
 void copyArray(struct List *list, struct Activity **aux);
 void freeNodeByAct(struct List *list, struct Activity *act);
+struct Person* createList(struct List *list, struct Person *person, struct Person **personArr);
+int updateList(struct Person *person, struct Activity *act);
+int deleteList(struct Person *person, struct Activity *act);
+void saveInArray(struct Person **array, struct Person *person);
+char * toStringPersonList(struct Person *person, struct Activity **aux);
 
 int main() {
 
@@ -69,67 +94,139 @@ int main() {
 	struct Activity *act9 = (struct Activity*)malloc(sizeof(struct Activity));
 	struct Activity *act10 = (struct Activity*)malloc(sizeof(struct Activity));
 	struct Activity *actUp = (struct Activity*)malloc(sizeof(struct Activity));
+
 	//Fill data of to-update activity
     actUp->id = 1001; actUp->title = "Activity Updated"; actUp->description = "Some Description updated"; actUp->priority = "Low";
     actUp->start.day = 10; actUp->start.month = 6; actUp->start.year = 2019;
 	actUp->finish.day = 11; actUp->finish.month = 6; actUp->finish.year =2019;
 
     //Initialize variables
-    struct List list;
-    list.index = 0;
-    list.head = NULL;
 	struct Activity **actDur; //New Node-pointer-array for dateSort
+	struct Person **personArr;
+	personArr = (struct Person**)malloc(PEOPLENUM * sizeof(struct Person*));
 
-    /*Calling functions */
-    fillData(&list, act1);
-    createNodeSort(&list, act1);
-    fillData(&list, act2);
-    createNodeSort(&list, act2);
-    fillData(&list, act3);
-    createNodeSort(&list, act3);
-    fillData(&list, act4);
-    createNodeSort(&list, act4);
-    fillData(&list, act5);
-    createNodeSort(&list, act5);
-    fillData(&list, act6);
-    createNodeSort(&list, act6);
-    fillData(&list, act7);
-    createNodeSort(&list, act7);
-    fillData(&list, act8);
-    createNodeSort(&list, act8);
-    fillData(&list, act9);
-    createNodeSort(&list, act9);
-    fillData(&list, act10);
-    createNodeSort(&list, act10);
+	//Create people
+	struct Person *newPerson1;
+	struct Person *newPerson2;
+	struct Person *newPerson3;
 
-	actDur = durationSort(&list);
-    printf("%s", toStringList(&list, actDur));
+	struct Person person1;
+	struct Person person2;
+	struct Person person3;
+	struct List list1;
+	list1.index = 0;
+	list1.head = NULL;
+	struct List list2;
+	list2.index = 0;
+	list2.head = NULL;
+	struct List list3;
+	list3.index = 0;
+	list3.head = NULL;
+
+	//Create people's lists
+	person1.name = "Alex";
+	person1.list = &list1;
+	fillData(&list1, act1);
+    createNodeSort(&list1, act1);
+    fillData(&list1, act2);
+    createNodeSort(&list1, act2);
+    fillData(&list1, act3);
+    createNodeSort(&list1, act3);
+
+    person2.name = "Jimmy";
+	person2.list = &list2;
+	fillData(&list2, act4);
+    createNodeSort(&list2, act4);
+    fillData(&list2, act5);
+    createNodeSort(&list2, act5);
+    fillData(&list2, act6);
+    createNodeSort(&list2, act6);
+
+    person3.name = "Amaury";
+	person3.list = &list3;
+	fillData(&list3, act7);
+    createNodeSort(&list3, act7);
+    fillData(&list3, act8);
+    createNodeSort(&list3, act8);
+    fillData(&list3, act9);
+    createNodeSort(&list3, act9);
+    fillData(&list3, act10);
+    createNodeSort(&list3, act10);
+
+    //Create
+    newPerson1 = createList(&list1, newPerson1, personArr);
+    actDur = dateSort(newPerson1->list);
+    printf("%s", toStringPersonList(newPerson1, actDur));
     free(actDur);
 
-    updateActivitySort(&list, actUp);
-    actDur = durationSort(&list);
-    printf("%s", toStringList(&list, actDur));
+    newPerson2 = createList(&list2, newPerson2, personArr);
+    actDur = dateSort(newPerson2->list);
+    printf("%s", toStringPersonList(newPerson2, actDur));
     free(actDur);
 
-    deleteActivity(&list, act2);
-    actDur = durationSort(&list);
-    printf("%s", toStringList(&list, actDur));
+    newPerson3 = createList(&list3, newPerson3, personArr);
+    actDur = dateSort(newPerson3->list);
+    printf("%s", toStringPersonList(newPerson3, actDur));
     free(actDur);
 
-	freeNodeByAct(&list, act1);
-	freeNodeByAct(&list, act2);
-	freeNodeByAct(&list, act3);
-	freeNodeByAct(&list, act4);
-	freeNodeByAct(&list, act5);
-	freeNodeByAct(&list, act6);
-	freeNodeByAct(&list, act7);
-	freeNodeByAct(&list, act8);
-	freeNodeByAct(&list, act9);
-	freeNodeByAct(&list, act10);
-	freeNodeByAct(&list, actUp);
+
+    //Update
+    updateList(newPerson1, actUp);
+    actDur = dateSort(newPerson1->list);
+    printf("%s", toStringPersonList(newPerson1, actDur));
+    free(actDur);
+
+    updateList(newPerson2, actUp);
+    actDur = dateSort(newPerson2->list);
+    printf("%s", toStringPersonList(newPerson2, actDur));
+    free(actDur);
+
+    updateList(newPerson3, actUp);
+    actDur = dateSort(newPerson3->list);
+    printf("%s", toStringPersonList(newPerson3, actDur));
+    free(actDur);
+
+
+    //Delete
+    deleteList(newPerson1, act2);
+    actDur = dateSort(newPerson1->list);
+    printf("%s", toStringPersonList(newPerson1, actDur));
+    free(actDur);
+
+    deleteList(newPerson2, act5);
+    actDur = dateSort(newPerson2->list);
+    printf("%s", toStringPersonList(newPerson2, actDur));
+    free(actDur);
+
+    deleteList(newPerson3, act9);
+    actDur = dateSort(newPerson3->list);
+    printf("%s", toStringPersonList(newPerson3, actDur));
+    free(actDur);
+
+    //Free
+    free(act1);
+    free(act2);
+    free(act3);
+    free(act4);
+    free(act5);
+    free(act6);
+    free(act7);
+    free(act8);
+    free(act9);
+    free(act10);
+    free(actUp);
+    free(newPerson1);
+    free(newPerson2);
+    free(newPerson3);
+
 }
 
-/*Save the activity at the end of the linked list */
+/**
+*@brief Save the activity at the end of the linked list
+*@param list A pointer to a list
+*@param act A pointer to an activity
+*@return A flag (True/False)
+*/
 int createNode(struct List *list, struct Activity *act){
     int flag = FALSE;
     struct Node *newNode;
@@ -156,7 +253,12 @@ int createNode(struct List *list, struct Activity *act){
     return flag;
 }
 
-/*Save the activity in the linked list sorting it by start date */
+/**
+*@brief Save the activity in the linked list sorting it by start date
+*@param list A pointer to a list
+*@param act A pointer to an activity
+*@return a flag (True/False)
+*/
 int createNodeSort(struct List *list, struct Activity *act){
 	int flag = FALSE;
     struct Node *newNode;
@@ -209,8 +311,13 @@ int createNodeSort(struct List *list, struct Activity *act){
 	return flag;
 }
 
-/*Update an activity
-Activity to update -> act1, ID= 1001*/
+/**
+*@brief Update an activity
+*@param list A pointer to a list
+*@param actUp A pointer to an activity to update
+*@return A flag (True/False)
+*/
+//Activity to update -> act1, ID= 1001
 int updateActivity(struct List *list, struct Activity *actUp){
     printf("\n\n-----UPDATE ACTIVITY 1001-----\n");
     int flag = FALSE;
@@ -223,7 +330,12 @@ int updateActivity(struct List *list, struct Activity *actUp){
     return flag;
 }
 
-/*Update an activity using the date sorting */
+/**
+*@brief Update an activity using the date sorting
+*@param list A pointer to a list
+*@param actUp A pointer to an activity to update
+*@return A flag (True/False)
+*/
 int updateActivitySort(struct List *list, struct Activity *actUp){
 	printf("\n\n-----UPDATE ACTIVITY 1001 (Sorting)-----\n");
     int flag = FALSE;
@@ -235,8 +347,12 @@ int updateActivitySort(struct List *list, struct Activity *actUp){
     return flag;
 }
 
-/*Delete an activity
-Delete act2, ID= 1002 */
+/**
+*@brief Delete an activity
+*@param list A pointer to a list
+*@param act A pointer to an activity to delete
+*@return A flag (True/False)
+*/
 int deleteActivity(struct List *list, struct Activity *act){
     printf("\n\n-----DELETE ACTIVITY 1002-----\n");
     int flag = FALSE;
@@ -264,7 +380,12 @@ int deleteActivity(struct List *list, struct Activity *act){
     return flag;
 }
 
-/*Searchs an activity's ID and return the node where its located */
+/**
+*@brief Searchs an activity's ID and return the node where its located
+*@param list A pointer to a list
+*@param act A pointer to a activity to find
+*@return A pointer to a node where the activity its located
+*/
 struct Node* findId(struct List *list, struct Activity *act){
     struct Node *node;
 	struct Node *nodeFound = NULL;
@@ -277,7 +398,11 @@ struct Node* findId(struct List *list, struct Activity *act){
     return nodeFound;
 }
 
-/*Returns the print format of a date */
+/**
+*@brief Returns the print format of a date
+*@param date A pointer to a date struct
+*@return A string with the date
+*/
 char* toStringDate(struct Date *date){
     char output[100];
     char tempInt[10];
@@ -295,7 +420,11 @@ char* toStringDate(struct Date *date){
     return output;
 }
 
-/*Returns the print format of an activity */
+/**
+*@brief Returns the print format of an activity
+*@param act A pointer to an activity
+*@return A string with the activity information
+*/
 char* toStringActivity(struct Activity *act){
     char output[1000];
     char tempId[10];
@@ -329,7 +458,12 @@ char* toStringActivity(struct Activity *act){
     return output;
 }
 
-/*Returns the print format of the list */
+/**
+*@brief Returns the print format of the list
+*@param list A pointer to a list
+*@param aux A pointer to the first element of an activity-pointer array
+*@return A string with the entire list information
+*/
 char* toStringList(struct List *list, struct Activity **aux){
     char output[10000];
     strcpy(output, "");
@@ -340,7 +474,11 @@ char* toStringList(struct List *list, struct Activity **aux){
     return output;
 }
 
-/*Fill the activitie's information*/
+/**
+*@brief Fill the activitie's information
+*@param list A pointer to a list
+*@param act A pointer to an activity
+*/
 void fillData(struct List *list, struct Activity *act) {
 	char temp_index[3];
 	strcpy(temp_index, "");
@@ -353,8 +491,9 @@ void fillData(struct List *list, struct Activity *act) {
 	act->finish.day = 28 - (list->index); act->finish.month = 6; act->finish.year = 2019;
 }
 
-/*Sort the activities by start date in ascending order
-	@return pointer to activity-pointer-array
+/**
+*@brief Sort the activities by start date in ascending order
+*@return pointer to activity-pointer-array
 */
 struct Activity ** dateSort(struct List *list){
 	int i, j, greater = 0;
@@ -377,10 +516,11 @@ struct Activity ** dateSort(struct List *list){
 	return auxDate;
 }
 
-/* Function to compare dates
-	@return 1 if date1 is bigger
-	@return 2 if date2 is bigger
-	@return -1 if date1 == date2
+/**
+*@brief Function to compare dates
+*@param act1 A pointer to an activity
+*@param act2 A pointer to an activity
+*@return A flag with the biggest date
  */
 int compareTo(struct Activity *act1, struct Activity *act2){
 	int flag = -1;
@@ -404,7 +544,11 @@ int compareTo(struct Activity *act1, struct Activity *act2){
 	return flag;
 }
 
-/* Sort the linked list by the activitie's duration */
+/**
+*@brief Sort the linked list by the activitie's duration
+*@param list A pointer to a list
+*@return The first element in an activity-pointer array
+*/
 struct Activity ** durationSort(struct List *list){
 	struct Activity **auxDur;
 	int duration1 = 0, duration2 = 0, i, j;
@@ -426,7 +570,11 @@ struct Activity ** durationSort(struct List *list){
 	return auxDur;
 }
 
-/*Get the duration of an activity */
+/**
+*@brief Get the duration of an activity
+*@param act A pointer to an activity
+*@return The duration in days of an activity
+*/
 int getDuration(struct Activity *act){
 	int duration;
 	int startDays = 0, finishDays = 0;
@@ -436,7 +584,11 @@ int getDuration(struct Activity *act){
 	return duration;
 }
 
-/* Get the number of days contained in the start date */
+/**
+*@brief Get the number of days contained in the start date
+*@param date A date struct
+*@return The number of days in the date
+*/
 int getDays(struct Date date){
 	int days = getLeapYears(date.year); //The variable "days" initialize with the extra days, including the actual year if its a leap year
 	int actualLeapYear = isLeapYear(date.year);
@@ -482,7 +634,11 @@ int getDays(struct Date date){
 	return days;
 }
 
-/*Get the leap years (extra days) between year 0 and the activity start year */
+/**
+*@brief Get the leap years (extra days) between year 0 and the activity start year
+*@param year A year from a date struct
+*@return The number of leap years between year 0 and the entered year
+*/
 int getLeapYears(int year){
 	int leapYears = 0;
 	int dif = 0, i;
@@ -496,7 +652,11 @@ int getLeapYears(int year){
 	return leapYears;
 }
 
-/*Function to know if the activity start year is a leap year */
+/**
+*@brief Function to know if the activity start year is a leap year
+*@param year A year from a date struct
+*@return A flag (True/False) if the actual year is a leap year
+*/
 int isLeapYear(int year){
 	int flag = FALSE;
 	if((year%4==0)&&((year%400 == 0)||(!(year%100 == 0)))){
@@ -505,7 +665,11 @@ int isLeapYear(int year){
 	return flag;
 }
 
-/*Copy the linked list to a new Activity-pointer-array*/
+/**
+*@brief Copy the linked list to a new Activity-pointer-array
+*@param list A pointer to a list
+*@param aux The first element of an activity-pointer array
+*/
 void copyArray(struct List *list, struct Activity **aux){
 	int i = 0;
 	struct Node *node;
@@ -516,7 +680,11 @@ void copyArray(struct List *list, struct Activity **aux){
 
 }
 
-/* Free a node by sending its activity */
+/**
+*@brief Free a node by sending its activity
+*@param list A pointer to a list
+*@param act A pointer to an activity
+*/
 void freeNodeByAct(struct List *list,struct Activity *act){
 	struct Node *node;
 	for(node = list->head; node!= NULL; node=node->next){
@@ -525,4 +693,78 @@ void freeNodeByAct(struct List *list,struct Activity *act){
 			break;
 		}
 	}
+}
+
+/**
+@brief Creates a person and his activity list
+*@param list A pointer to a list
+*@param person A pointer to a person
+*@param personArr The first element in a person-pointer array
+*@return A pointer to a person
+*/
+struct Person* createList(struct List *list, struct Person *person, struct Person **personArr){
+	struct Person *newPerson;
+	newPerson = (struct Person *)malloc(sizeof(struct Person));
+	newPerson->name = person->name;
+	newPerson->list = list;
+
+	saveInArray(personArr, newPerson);
+
+	return newPerson;
+}
+
+/**
+*@brief Updates a person list and name
+*@param person A pointer to a person
+*@param act A pointer to an activity
+*@return A flag (True/False)
+*/
+int updateList(struct Person *person, struct Activity *act){
+	int flag = FALSE;
+	char *aux = "updated";
+	flag = updateActivitySort(person->list, act);
+	strcat(person->name, aux);
+
+	return flag;
+}
+
+/**
+*@brief Delete an activity from a person list
+*@param person A pointer to a person
+*@param act A pointer to an activity
+*@return A flag (True/False)
+*/
+int deleteList(struct Person *person, struct Activity *act){
+	int flag = FALSE;
+	flag = deleteActivity(person->list, act);
+	return flag;
+}
+
+/**
+*@brief Function to save a person in a person-pointer-array
+*@param array The first element in a person-pointer array
+*@param person A pointer to a person
+*/
+void saveInArray(struct Person **array, struct Person *person){
+	int i = 0;
+	while(*(array + i) != NULL){
+		i++;
+	}
+	*(array + i) = person;
+}
+
+/**
+*@brief Put the person information on a string
+*@param person A pointer to a person
+*@param aux The first element in an activity-pointer array
+*@return A string with the person's information
+*/
+char * toStringPersonList(struct Person *person, struct Activity **aux){
+	char output[100000];
+	strcpy(output, "");
+	strcat(output, "NAME: ");
+	strcat(output, person->name);
+	strcat(output, "\n");
+	strcat(output, toStringList(person->list, aux));
+	return output;
 }
